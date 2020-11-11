1851510fc74de699824ac9b8264cb6063c63941d
Gabriel Morales <grmorale@gmail.com>
2020-11-11 15:07:33 -0800

    massively restructure file and alter app's purpose for practicality

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express(),
+    port = process.env.PORT || 3000;
+//app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/views"));
+
+app.get("/", function(req,res){
+    res.sendFile("index.html");
+});
+
+app.listen(port,function(){
+    console.log("hola amigo!");
+});
